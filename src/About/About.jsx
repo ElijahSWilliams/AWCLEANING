6e8d31f0319ac60f358bfc8488791b6b0eed5409
Elijah <xrealntrue16x@gmail.com>
2026-04-04 12:16:35 -0400

    About Section Alomst Done - just resize picture on 768px

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,8 +1,10 @@
+import "./About.css";
+
 function About() {
   return (
     <div className="about">
-      <h1>About Us</h1>
-      <p>
+      <h1 className="about__title">About Us</h1>
+      <p className="about__main-text">
         At AWCleaning, we are dedicated to providing top-notch cleaning services
         that exceed our clients' expectations. With years of experience in the
         industry, we have built a reputation for reliability, professionalism,
@@ -14,6 +16,23 @@ function About() {
         cleaning, or specialized services, AWCleaning is here to make your life
         easier and your space cleaner.
       </p>
+      <p className="about__main-text">
+        {" "}
+        Service Areas: We proudly serve the Philadelphia, PA and the surrounding
+        areas. If you're located in any of these areas, we're just a call away
+        from providing you with exceptional cleaning services. Contact us today
+        to schedule your cleaning appointment and experience the AWCleaning
+        difference!{" "}
+      </p>
+
+      {/* Image of a Philadelphia Map */}
+      <div className="about__image-container">
+        <img
+          className="about__img"
+          src="https://upload.wikimedia.org/wikipedia/commons/2/28/Street_map_of_Philadelphia_and_surrounding_area.png"
+          alt="map of philadelphia"
+        />
+      </div>
     </div>
   );
 }
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import "./Header.css";
 
 function Header() {
+  /* State Variables */
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
   return (
     <header className="header">
       <h1 className="logo">AWCleaning</h1>
+
+      {/* Hamburger Menu Icon */}
+      <button className="hamburger" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        =
+      </button>
       <nav className="nav">
-        <ul>
+        <ul className={isMenuOpen ? "active" : ""}>
           <li>
             <a href="/">Home</a>
           </li>
